e41fdd0c5b93b6ff6550087f02c1c26a406f40e9
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2022-10-27 21:11:02 -0500

    Ejercicio 1 Completado

---
+++ Ejercicio1.py
@@ -0,0 +1,42 @@
+numero = int(input("Ingrese un número: ")) 
+
+#Opcion 1
+esPar = numero % 2 == 0
+if numero == 0:
+    print("Neutro")
+elif esPar and numero > 0:
+    print("Positivo Par")
+elif not esPar and numero > 0:
+    print("Positivo Impar")
+elif esPar and numero < 0:
+    print("Negativo Par")
+elif not esPar and numero < 0:
+    print("Negativo Impar")
+    
+# Opcion 2
+if numero == 0:
+    print("Neutro")
+else:
+    esPar = numero % 2 == 0
+    if esPar :
+        if numero > 0:
+            print("Positivo par")  
+        else:
+            print("Negativo par")  
+    else:
+        if numero > 0:
+            print("Positivo impar")       
+        else:
+            print("Negativo impar")  
+               
+
+
+
+
+    
+    
+    
+
+
+    
+    
